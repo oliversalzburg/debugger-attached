3ad93f11ecd1c747fed332e2d7bd688c0419878e
Oliver Salzburg <oliver.salzburg@gmail.com>
2017-08-22 12:48:54 +0200

    [FEATURE] Detect debugger from CLI args

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 "use strict";
 
 function isDebuggerAttached() {
-	return typeof v8debug === "object";
+	return typeof v8debug === "object" || /--debug|--inspect/.test( process.execArgv.join( " " ) );
 }
 
 module.exports = isDebuggerAttached;
